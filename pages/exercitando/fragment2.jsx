c80afc76e5e17b63d62ea477ec1cd386f0b8b56d
Leticia Lopes <107498937+LeticiaLopess@users.noreply.github.com>
2022-12-03 16:36:51 -0300

    fragmento

---
+++ pages/exercitando/fragment2.jsx
@@ -0,0 +1,10 @@
+export default function fragment () {
+    return (
+        <>
+        <h1>Título</h1>
+        <h2>Subtítulo</h2>
+        </>
+    )
+}
+
+// envolvimento com tag vazia para corrigir o erro
