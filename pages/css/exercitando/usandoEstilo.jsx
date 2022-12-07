58a2f863255d1f627b03effbbe55407519280596
Leticia Lopes <107498937+LeticiaLopess@users.noreply.github.com>
2022-12-07 17:15:54 -0300

    aplicando estilos de forma condicional

---
+++ pages/css/exercitando/usandoEstilo.jsx
@@ -0,0 +1,11 @@
+import Estilo from "../../../components/exercitando/Estilo";
+
+
+export default function usandoEstilo() {
+   return (
+    <div>
+        <Estilo numero = {3} color = "black" direita/>
+        <Estilo numero = {-13} color = "white" />
+    </div>
+   )
+}
