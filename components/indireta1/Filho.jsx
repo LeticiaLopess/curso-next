75c510715857edef62bf08e25de896cd9bba89e5
Leticia Lopes <107498937+LeticiaLopess@users.noreply.github.com>
2022-12-11 18:42:10 -0300

    comunicação indireta

--- components/indireta1/Filho.jsx
+++ components/indireta1/Filho.jsx
@@ -1,15 +1,22 @@
 export default function Filho(props) {
-    // 
     console.log(props.funcao)
     return (
         <div>
-            <h1>Filho</h1>
+            <h1>Filho aa</h1>
             <button onClick={props.funcao}>Falar com o Pai #01</button>
             <button onClick={
+
                 () => props.funcao("Passei no ENEM!", "Uhuuuu", 10)
-            }>
-                Falar com o Pai #02
-            </button>
+                
+                }>
+                    Falar com o Pai #02
+                </button>
         </div>
     )
 }
+
+/*
+<button onClick={props.funcao}>Falar com o Pai #01</button> = COMUNICAÇÃO DIRETA
+
+<button onClick={() => props.funcao("Passei no ENEM!")}>Falar com o Pai #02</button> = COMUNICAÇÃO INDIRETA através de uma função
+*/
--- components/indireta1/Pai.jsx
+++ components/indireta1/Pai.jsx
@@ -8,7 +8,17 @@ export default function Pai(props) {
 
     return (
         <div>
-            <Filho funcao={falarComigo} />
+            <Filho funcao={falarComigo} /> 
         </div>
     )
 }
+
+// como passar uma informação do componente filho para o componente pai sendo que  filho não tem nenhuma ligação com pai
+
+// para haver comunicação indireta, antes deve ter comunicação direta. O pai deve passar alguma coisa pro filho para que o filho responda quando necessário
+
+// ao invés de passarmos uma string/número, passamos uma função como parâmentro de um componente para outro
+
+/*
+<Filho funcao={falarComigo} = comunicação direta
+*/
