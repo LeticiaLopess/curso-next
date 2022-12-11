63a9506083fab77126cc075a55aa4a98f44d6645
Leticia Lopes <107498937+LeticiaLopess@users.noreply.github.com>
2022-12-11 19:40:58 -0300

    componente com estado

--- pages/estado/mouse.jsx
+++ pages/estado/mouse.jsx
@@ -1,21 +1,22 @@
 import { useState } from "react"
 
-export default function mouse() {
-    // React Hooks
-    const [x, setX] = useState(0)
+export default function mouse () {
+    const [x, asetX] = useState(0) // escrito em uma linha
 
-    const arrayY = useState(0)
+    const arrayY = useState(0)     // escrito em mais linhas
     let y = arrayY[0]
     const alterarY = arrayY[1]
 
-    const estilo = {
+
+   
+    const estilo = { 
         display: "flex",
         flexDirection: "column",
         justifyContent: "center",
-        alignItems: "center",
-        backgroundColor: "#222",
+        backgroundColor: "#222", 
         color: "#fff",
         height: "100vh"
+        
     }
 
     function quandoMover(ev) {
@@ -29,4 +30,25 @@ export default function mouse() {
             <span>Eixo Y: {y}</span>
         </div>
     )
+    }
+
+
+// clientX e clientY - propriedades que aparecem no console no inspecionar
+
+// não posso alterar o valor direto mesmo que eu esteja mexendo no x e no y que foi gerado pelo useState. Por isso não posso fazer assim:
+/* function quandoMover(ev) {
+    x = ev.clientX
+    y = ev.clientY
+    console.log('valor alterado')
 }
+
+tenho que usar as funções alterarX e alterarY
+
+const arrayY = useState(0)
+    let y = arrayY[0]
+    const alterarY = arrayY[1]
+
+    TRANFORMANDO EM UMA ÚNICA LINHA:
+
+const [y, alterarY] = useState(0)
+*/
