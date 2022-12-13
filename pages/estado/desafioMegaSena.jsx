fc1fadd5ccd0993238463b89f690e110e272656d
Leticia Lopes <107498937+LeticiaLopess@users.noreply.github.com>
2022-12-13 11:12:02 -0300

    utilizando useEffect

--- pages/estado/desafioMegaSena.jsx
+++ pages/estado/desafioMegaSena.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import NumberDisplay from "../../components/NumberDisplay"
 import { mega } from "../../functions/mega"
 
@@ -6,8 +6,11 @@ import { mega } from "../../functions/mega"
 export default function megasena() {
 
     const [qtde, setQtde] = useState(6)
-    const [numeros, setNumeros] = useState(mega(qtde))
+    const [numeros, setNumeros] = useState([])
 
+    useEffect(() => {
+        setNumeros(mega())
+    }, [])
 
     function renderNumeros() {
         return numeros.map(
